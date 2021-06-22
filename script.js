13152468c6a60c6d592f84e63cadf21f65cfd2ea
Shadi Jaafar <shadinjaafar@gmail.com>
2021-06-22 19:05:39 -0400

    New interface

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+const main = document.getElementById('main');
 const container = document.getElementsByClassName('container')[0];
+
+const reset = document.createElement('button');
+reset.classList.add('button');
+reset.innerText = 'RESET';
+main.appendChild(reset);
+
+
 const grid = document.createElement('div');
 grid.classList.add('grid');
 container.appendChild(grid);
 
-let x = 5;
+let x = 16;
 
 createGrid(x*x);
 
@@ -13,5 +21,16 @@ function createGrid(number) {
         cell.classList.add('cell');
         grid.appendChild(cell);
         console.log(cell)
+        cell.addEventListener('mouseover', () => {
+            cell.classList.add('blue');
+        })
     };
 }
+
+reset.addEventListener('click', () => {
+    const gridArray = Array.from(grid.childNodes);
+    gridArray.forEach((element) => {
+        grid.removeChild(element);
+    });
+    createGrid(16*16);
+});
